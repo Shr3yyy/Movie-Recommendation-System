c8968335c84460162ebc2a6522538dfff0016930
SHREYANSH BHARDWAJ <shreyyy3126@gmail.com>
2026-01-08 18:20:24 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,17 @@
+// File: pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="Discover and explore movies with our recommendation system" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
